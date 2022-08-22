95a5c59526497acd843621f8e9d5aa5214b8e589
Lobinux <lobinux@digilearnnials.com>
2022-08-22 17:32:40 -0300

    CarController.cs is ok

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    public List<WheelCollider> throttleWheels = new List<WheelCollider>();
+    public List<WheelCollider> steeringWheels = new List<WheelCollider>();
+    public float throttleCoefficient = 20000f;
+    public float maxTurn = 20f;
+    private float giro = 0f;
+    private float acel = 1f;
+
+	void FixedUpdate () 
+    {
+        foreach (var wheel in throttleWheels) 
+        {
+            wheel.motorTorque = throttleCoefficient * Time.deltaTime * acel;
+        }
+        foreach (var wheel in steeringWheels) 
+        {
+            wheel.steerAngle = maxTurn * giro;
+        }
+        giro = 0f;
+    }
+
+    public void SetGiro(float giro) => this.giro = giro;
+    public void SetAcel(float val) => acel = val;
+}
