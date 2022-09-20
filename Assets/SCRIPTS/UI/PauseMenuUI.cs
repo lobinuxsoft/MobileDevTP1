321dc3673073a525cc254c847156e7d2e0bc7cd1
Lobinux <lobinux@digilearnnials.com>
2022-09-20 00:55:15 -0300

    Add menu pause and new art for UI, build for Windows and Android

---
+++ Assets/SCRIPTS/UI/PauseMenuUI.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class PauseMenuUI : MonoBehaviour
+{
+    CanvasGroup canvasGroup;
+
+    private void Awake() => canvasGroup = GetComponent<CanvasGroup>();
+
+    public void Pause()
+    {
+        if(Time.timeScale > 0)
+        {
+            ShowMenu();
+        }
+        else
+        {
+            HideMenu();
+        }
+    }
+
+    private void ShowMenu()
+    {
+        Time.timeScale = 0;
+
+        canvasGroup.alpha = 1;
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.interactable = true;
+    }
+
+    private void HideMenu()
+    {
+        Time.timeScale = 1;
+
+        canvasGroup.alpha = 0;
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.interactable = false;
+    }
+
+    public void ReturnToMainMenu()
+    {
+        HideMenu();
+
+        SceneManager.LoadScene(0);
+    }
+}
